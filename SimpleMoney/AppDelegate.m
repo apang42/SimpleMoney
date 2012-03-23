72ec9d38f2c49713bab28a9c598d65e7da493ea9
Arthur Pang <apang42@gmail.com>
2012-03-22 17:29:08 -0700

    merged with friendly-error-messages branch

--- SimpleMoney/AppDelegate.m
+++ SimpleMoney/AppDelegate.m
@@ -8,8 +8,8 @@
 
 #import "AppDelegate.h"
 
-// #define kSERVICE_URL @"http://severe-leaf-6733.herokuapp.com/"
-#define kSERVICE_URL @"http://192.168.0.106:3000"
+#define kSERVICE_URL @"http://severe-leaf-6733.herokuapp.com/"
+//#define kSERVICE_URL @"http://192.168.0.106:3000"
 
 @implementation AppDelegate
 @synthesize window = _window;
